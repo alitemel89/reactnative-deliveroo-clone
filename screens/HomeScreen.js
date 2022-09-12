6151a0ba2997c39432b93a605f7de3e245e72739
alitemel89 <alitemel89.ce@gmail.com>
2022-09-12 19:27:42 +0300

    finish configuration with tailwind css

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text className="bg-slate-400 text-red-200">HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
